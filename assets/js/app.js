e47e25ebf80b80743cae1cf0b12a968957adf908
David Magalhães <david.williamx@gmail.com>
2024-08-21 12:00:45 -0300

    Adiciona bootstrap icons, cria elementos para tratamento de erros.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,42 @@
-
 let peso = document.querySelector("#inputPeso");
 let altura = document.querySelector("#inputAltura");
-const button = document.querySelector("#botao");
+const botaoCalcular = document.querySelector("#botaoCalcular");
+const botaoResetar = document.querySelector("#botaoReset");
 let imc = document.getElementById("imc");
-const textoPadrao = document.querySelector(".texto-padrao");
-const alertaPadrao = document.querySelector(".alerta-padrao");
-const alertaInfo = document.querySelector(".alerta-info");
-const alertaSucesso = document.querySelector(".alerta-sucesso");
-const alertaAtencao = document.querySelector(".alerta-atencao");
-const alertaUrgente = document.querySelector(".alerta-urgente");
 
-peso.value = "";
-altura.value = "";
-
-button.addEventListener("click",fractionDigits => {
+function calcularIMC(peso, altura) {
     let valorPeso = Number(peso.value);
     let valorAltura = Number(altura.value);
-    console.log('Valor de peso alterado - ' + valorPeso);
-    console.log('Valor de altura alterado - ' + valorAltura);
+    const isNumberUndefined = valorPeso === undefined || valorAltura === undefined;
+    const isNotNumber = isNaN(valorPeso) || isNaN(valorAltura);
+    const isNumberZero = valorPeso < 0 || valorAltura < 0;
+    const isNumberMax = valorPeso >= 300 || valorAltura >= 3.0;
 
-    //console.log(((valorPeso / valorAltura ** 2)*10000).toFixed(1));
+    if (isNumberUndefined) {
+        return "Vazio - Não foram informados valores como parametros";
+    } else if (isNotNumber) {
+        return "Não é um número - Números devem ser informados como parametros";
+    } else if (isNumberZero) {
+        return "Valor Negativo - Números negativos não são válidos";
+    } else if (isNumberMax) {
+        return "Valor máximo atingido - Números informados estão fora do limite";
+    } else {
+        return Number((valorPeso / valorAltura ** 2).toFixed(2));
+    }
+}
 
-    imc.innerHTML = ((valorPeso / valorAltura ** 2)*10000).toFixed(1);
+function resetar() {
+    peso.value = "";
+    altura.value = "";
+}
+
+botaoResetar.addEventListener("click", () => {
+    resetar();
+});
+
+botaoCalcular.addEventListener("click", () => {
+
+    imc.innerHTML = ((valorPeso / valorAltura ** 2) * 10000).toFixed(1);
     textoPadrao.classList.add('d-none');
     alertaAtencao.classList.remove('d-none');
 });
